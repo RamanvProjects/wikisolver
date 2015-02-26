8f7abf99c7ab94d6d3cf636caa483927d49fa824
Vivek Ramanujan <ramanv@cs.washington.edu>
2015-02-25 20:44:24 -0800

    Added crawl method and a preliminary solve method

--- wikitools.py
+++ wikitools.py
@@ -1,6 +1,7 @@
 import urllib
 import json
 import time
+
 def is_number(s):
     try:
         float(s)
@@ -25,8 +26,13 @@ class WikiSolver:
 		json_data = json.loads(jsonStr)
 		return json_data
 
-	def crawl(self, initial_link):
-		pass
+	def crawl(self, ID, depth):
+		json_links = self.get_all_IDs(ID)
+
+		for ID in json_links:
+			IDs_in_ID = self.get_all_IDs(ID)
+			for subID in IDs_in_ID:
+				print(self.extract_words(subID))
 
 	def get_page_id(self, wiki_link):
 		source = self.get_source(wiki_link)
@@ -53,20 +59,62 @@ class WikiSolver:
 	def get_all_IDs(self, ID):
 		link = self.get_API_link(ID)
 		source = self.get_source(link)
-		IDs = self.json_to_dict(source)['query']['pages'].keys()
-		return IDs
+		IDs = self.json_to_dict(source)
+		if 'query' in IDs:
+			IDs = IDs['query']['pages'].keys()
+			return IDs
+		else:
+			return None
 
 	def extract_words(self, ID):
 		link = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&pageids=" + ID
 		return self.json_to_dict(self.get_source(link))
 
+	def solve(self, init_search, end_search, max_depth):# initial = self.linkInit, final = self.linkFinal):
+		ID_init = self.get_page_id(self.search_link(init_search))
+		ID_end = self.get_page_id(self.search_link(end_search))
+
+		graph = {
+					ID_init : self.get_all_IDs(ID_init)
+				}
+
+		for ID in graph.keys():
+			for subID in graph[ID]:
+				if not subID in graph:
+					graph[subID] = self.get_all_IDs(subID)
+
+		print graph
+
+	def backtrace(self, parent, start, end):
+	    path = [end]
+
+	    while path[-1] != start:
+	        path.append(parent[path[-1]])
+	    path.reverse()
+	    return path
+
+	def solves(self, ID_init, ID_end, max_depth, graph):
+
+		for ID_key in graph.keys():
+			for ID in graph[ID_key]:
+				if not ID in graph:
+					graph[ID] = self.get_all_IDs(ID)
+
+			if ID_end in graph:
+				print graph
+				return "I HATE YOUR GUTS" 
+
+		print graph
+
+
 w = WikiSolver("Isaac Newton", "Apple")
 #print w.get_source("http://en.wikipedia.org/w/api.php?action=query&format=json&pageids=14627&generator=links&gpllimit=max")
 
 #print w.get_page_id(w.search_link("Isaac Newton"))
-link = w.search_link("Isaac Newton")
+link = w.search_link("Apple")
 ID = w.get_page_id(link)
 links_on_page = w.get_all_IDs(ID)
 words = w.extract_words(ID)
 print words
-
+#print w.extract_words(w.get_page_id(w.generate_link(links_on_page[3])))
+print w.solve("Isaac Newton", "Atom", 2)
